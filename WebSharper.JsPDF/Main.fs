23ecec5b7251e252ae1b81b9b04102d80e02ea51
GyGerg <gyalus.gergo.98@gmail.com>
2023-09-11 14:49:26 +0200

    AcroForm, AddImage, Annotations, AutoPrint, Arabic plugin bindings

--- WebSharper.JsPDF/Main.fs
+++ WebSharper.JsPDF/Main.fs
@@ -25,24 +25,6 @@ open WebSharper.InterfaceGenerator
 module Definition =
     
             
-    let AnnotationType = Pattern.EnumStrings "AnnotationType" [
-        "text"
-        "freetext"
-        "link"
-    ]
-    let Annotation =
-        Class "Annotation"
-        |+> Static [
-            Constructor (
-                AnnotationType
-                * T<string>?title
-                * T<string>?contents
-                * (!?T<bool>?``open``)
-                * (!?T<string>?name)
-                * (!?T<float>?top)
-                * (!?T<int>?pageNumber)
-            )
-        ]
     let OrientationPattern =
         Pattern.EnumStrings "orientation" [
             "portrait"
@@ -614,10 +596,277 @@ module Definition =
         ]
 
     module Plugins =
+        module AddImage =
+            
+            let imageDataTypes = T<string> + T<JavaScript.HTMLImageElement> + T<JavaScript.HTMLCanvasElement> + T<JavaScript.Uint8Array>
+            
+            let RGBAData = Pattern.Config "RGBAData" {
+                Required = [
+                    "data", T<JavaScript.Uint8ClampedArray>
+                    "width", T<float>
+                    "height", T<float>
+                ]
+                Optional = []
+            }
+            
+            let ImageCompression = Pattern.EnumStrings "ImageCompression" [
+                "NONE"
+                "FAST"
+                "MEDIUM"
+                "SLOW"
+            ]
+            
+            let ColorSpace = Pattern.EnumStrings "ColorSpace" [
+                "DeviceRGB"
+                "DeviceGray"
+                "DeviceCMYK"
+                "CalGray"
+                "CalRGB"
+                "Lab"
+                "ICCBased"
+                "Indexed"
+                "Pattern"
+                "Separation"
+                "DeviceN"
+            ]
+            
+            let ImageFormat = Pattern.EnumStrings "ImageFormat" [
+                "RGBA"
+                "UNKNOWN"
+                "PNG"
+                "TIFF"
+                "JPG"
+                "JPEG"
+                "JPEG2000"
+                "GIF87a"
+                "GIF89a"
+                "WEBP"
+                "BMP"
+            ]
+            
+            let ImageOptions = Pattern.Config "ImageOptions" {
+                Required = [
+                    "imageData", imageDataTypes+RGBAData.Type
+                    "x", T<float>
+                    "y", T<float>
+                    "width", T<float>
+                    "height", T<float>
+                ]
+                Optional = [
+                    "alias", T<string>
+                    "compression", ImageCompression.Type
+                    "rotation", T<float>
+                    "format", ImageFormat.Type
+                ]
+            }
+            
+            let ImageProperties = Pattern.Config "ImageProperties" {
+                Required = [
+                    "alias", T<float>
+                    "width", T<float>
+                    "height", T<float>
+                    "colorSpace", ColorSpace.Type
+                    "bitsPerComponent", T<int>
+                    "filter", T<string>
+                    "index", T<int>
+                    "data", T<string>
+                    "fileType", ImageFormat.Type
+                ]
+                Optional = [
+                    "decodeParameters", T<string>
+                    "transparency", T<obj>
+                    "palette", T<obj>
+                    "sMask", T<obj>
+                    "predictor", T<float>
+                    
+                ]
+            }
+            
+            let typesToExport = [
+                RGBAData
+                ImageCompression
+                ColorSpace
+                ImageFormat
+                ImageOptions
+                ImageProperties
+            ]
+            let methodsToApply = [
+                "addImage" =>
+                    (
+                             (imageDataTypes+RGBAData.Type)?imageData
+                             * T<string>?format
+                             * T<float>?x * T<float>?y
+                             * T<float>?w * T<float>?h
+                             * (!? T<string>)?alias
+                             * (!? ImageCompression.Type)?compression
+                             * (!? T<float>)?rotation
+                    ) ^-> JsPDFClass
+                "addImage" => ImageOptions.Type?options ^-> JsPDFClass
+                "getImageProperties" => imageDataTypes?imageData ^-> ImageProperties
+            ]
+        
+        module Arabic =
+            
+            let methodsToApply = [
+                "processArabic" => T<string>?text ^-> T<string>
+            ]
+            
+        module Annotations =
+            
+            let AnnotationType = Pattern.EnumStrings "AnnotationType" [
+                "text"
+                "freetext"
+                "link"
+            ]
+            let Annotation =
+                Class "Annotation"
+                |+> Static [
+                    Constructor (
+                        AnnotationType
+                        * T<string>?title
+                        * T<string>?contents
+                        * (!?T<bool>?``open``)
+                        * (!?T<string>?name)
+                        * (!?T<float>?top)
+                        * (!?T<int>?pageNumber)
+                    )
+                ]
+                
+            let typesToExport = [
+                AnnotationType
+                Annotation
+            ]
+            let methodsToApply = [
+                "createAnnotation" => Annotation?options ^-> T<unit>
+                "link" => T<float>?x * T<float>?y * T<float>?w * T<float>?h * T<obj>?options ^-> T<unit>
+                "textWithLink" => T<string>?text * T<float>?x * T<float>?y * T<obj>?options ^-> T<int>
+                "getTextWidth" => T<string>?text ^-> T<int>
+            ]
+            
         module AutoPrint =
             let AutoPrintInput = Pattern.EnumStrings "AutoPrintInput" ["non-conform";"javascript"]
             
             let typesToExport = [AutoPrintInput]
+            let methodsToApply = [
+                "autoPrint" => !?AutoPrintInput?options ^-> JsPDFClass
+            ]
+        
+        module AcroForm =
+            let TextAlign = Pattern.EnumStrings "TextAlign" [
+                "left";"center";"right"
+            ]
+            
+            let AcroFormAppearanceState = Pattern.EnumStrings "AcroFormAppearanceState" [
+                "On";"Off"
+            ]
+            
+            let AcroFormField =
+                Class "AcroFormField"
+                |+> Instance [
+                    "showWhenPrinted" =@ T<bool>
+                    "x" =@ T<float>
+                    "y" =@ T<float>
+                    "width" =@ T<float>
+                    "height" =@ T<float>
+                    "fieldName" =@ T<string>
+                    "fontName" =@ T<string>
+                    "fontStyle" =@ T<string>
+                    "fontSize" =@ T<int>
+                    "maxFontSize" =@ T<int>
+                    "color" =@ T<string>
+                    "defaultValue" =@ T<string>
+                    "value" =@ T<string>
+                    for n in ["hasAnnotation"; "readOnly"; "required"; "noExport"] do
+                        n =@ T<bool>
+                    "textAlign" =@ TextAlign.Type
+                ]
+                |+> Static [Constructor T<unit>]
+            
+            let AcroFormChoiceField =
+                Class "AcroFormChoiceField"
+                |+> Instance [
+                    for n in ["combo";"edit";"sort";"multiSelect";"doNotSpellCheck";"commitOnSelChange"] do
+                            n =@ T<bool>
+                    "topIndex" =@ T<int>
+                    "getOptions" => T<unit> ^-> T<string> 
+                    "setOptions" => Type.ArrayOf(T<string>)?value ^-> T<unit>
+                    "addOption" => T<string>?value ^-> T<unit>
+                    "removeOption" => T<string>?value * T<bool>?allEntries ^-> T<unit>
+                ]
+                |=> Inherits AcroFormField
+            let AcroFormListBox = Class "AcroFormListBox" |=> Inherits AcroFormChoiceField
+            let AcroFormComboBox = Class "AcroFormComboBox" |=> Inherits AcroFormChoiceField
+            let AcroFormEditBox = Class "AcroFormEditBox" |=> Inherits AcroFormChoiceField
+            
+            let AcroFormButton =
+                Class "AcroFormButton"
+                |+> Instance [
+                    for n in ["noToggleToOff";"radio";"pushButton";"radioIsUnison"] do
+                        n =@ T<bool>
+                    "caption" =@ T<string>
+                    "appearanceState" =@ AcroFormAppearanceState
+                ]
+            
+            let AcroFormPushButton = Class "AcroFormPushButton" |=> Inherits AcroFormButton
+            
+            let AcroFormChildClass =
+                Class "AcroFormChildClass"
+                |+> Instance [
+                    "Parent" =@ T<obj>
+                    "optionName" =@ T<string>
+                    "caption" =@ T<string>
+                    "appearanceState" =@ AcroFormAppearanceState.Type
+                ]
+                |=> Inherits AcroFormField
+            
+            let AcroFormRadioButton =
+                Class "AcroFormRadioButton"
+                |+> Instance [
+                    "setAppearance" => T<string>?appearance ^-> T<unit>
+                    "createOption" => T<string>?name ^-> AcroFormChildClass
+                ]
+                |=> Inherits AcroFormButton
+            
+            let AcroFormCheckBox =
+                Class "AcroFormCheckBox"
+                |+> Instance [
+                    "appearanceState" =@ AcroFormAppearanceState.Type
+                ]
+                |=> Inherits AcroFormButton
+                
+            let AcroFormTextField =
+                Class "AcroFormTextField"
+                |+> Instance [
+                    for n in ["multiline";"fileSelect";"doNotSpellCheck";"doNotScroll";"comb";"richText";"hasAppearanceStream"] do
+                        n =@ T<bool>
+                    "maxLength" =@ T<int>
+                    
+                ]
+                |=> Inherits AcroFormField
+            let AcroFormPasswordField =
+                Class "AcroFormPasswordField"
+                |=> Inherits AcroFormTextField
+            
+            let typesToExport = [
+                TextAlign
+                AcroFormAppearanceState
+                AcroFormField
+                AcroFormChoiceField
+                AcroFormListBox
+                AcroFormComboBox
+                AcroFormEditBox
+                AcroFormButton
+                AcroFormPushButton
+                AcroFormChildClass
+                AcroFormRadioButton
+                AcroFormCheckBox
+                AcroFormTextField
+                AcroFormPasswordField
+            ]
+            let methodsToApply = [
+                "addField" => AcroFormField?field ^-> JsPDFClass
+            ]
+            
         
         module Html =
             let Html2CanvasOptions = Pattern.Config "Html2CanvasOptions" {
@@ -772,12 +1021,20 @@ module Definition =
             ]
             
         let typesToExport = [
+            for t in AddImage.typesToExport do t
+            for t in Annotations.typesToExport do t
+            for t in AcroForm.typesToExport do t
             for t in Html.typesToExport do t
             for t in AutoPrint.typesToExport do t
         ]
         let methodsToApply =
             [
+                for m in AddImage.methodsToApply do m
+                for m in Arabic.methodsToApply do m
+                for m in Annotations.methodsToApply do m
+                for m in AcroForm.methodsToApply do m
                 for m in Html.methodsToApply do m
+                for m in AutoPrint.methodsToApply do m
             ]
     
     JsPDFClass
